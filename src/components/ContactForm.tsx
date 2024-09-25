5231c7869da466658b9842e432ef74a40dbc5cdc
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-09-25 14:18:03 +0200

    remove REACT_APP_ prefix to make it work on vercel

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
-import { useRef, type FormEvent } from 'react';
-import emailjs from '@emailjs/browser';
+import { useRef, type FormEvent } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const formRef = useRef<HTMLFormElement>(null);
@@ -7,18 +7,13 @@ const ContactForm = () => {
   const sendEmail = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const service_id = process.env.REACT_APP_EMAILJS_SERVICE_ID!
-    const template_id = "template_sb4p7te"
-    const formElement = event.currentTarget
-    const public_api_key = process.env.REACT_APP_EMAILJS_PUBLIC_API_KEY
+    const service_id = process.env.EMAILJS_SERVICE_ID!;
+    const template_id = "template_sb4p7te";
+    const formElement = event.currentTarget;
+    const public_api_key = process.env.EMAILJS_PUBLIC_API_KEY;
 
     emailjs
-      .sendForm(
-        service_id,
-        template_id,
-        formElement,
-        public_api_key
-      )
+      .sendForm(service_id, template_id, formElement, public_api_key)
       .then((result) => {
         console.log(result.text);
       })
@@ -26,19 +21,18 @@ const ContactForm = () => {
         console.log(error.text);
       });
 
-    formRef.current!.reset()
-  }
+    formRef.current!.reset();
+  };
 
   return (
-    <form onSubmit={sendEmail} className="flex flex-col p-4 mt-10 max-w-[500px] mx-auto" ref={formRef}>
+    <form
+      onSubmit={sendEmail}
+      className="flex flex-col p-4 mt-10 max-w-[500px] mx-auto"
+      ref={formRef}
+    >
       <h2 className="text-center text-lg text-slate-400 mb-2">EMAIL</h2>
       <div className="flex flex-col gap-4">
-        <input
-          id="email"
-          name="email"
-          className="input"
-          placeholder="Email"
-        />
+        <input id="email" name="email" className="input" placeholder="Email" />
         <textarea
           id="message"
           name="message"
@@ -46,8 +40,13 @@ const ContactForm = () => {
           placeholder="Message"
         />
       </div>
-      <button type="submit" className="card button m-auto mt-4 py-2 px-7 text-lg">Send</button>
-    </form >
+      <button
+        type="submit"
+        className="card button m-auto mt-4 py-2 px-7 text-lg"
+      >
+        Send
+      </button>
+    </form>
   );
 };
 
